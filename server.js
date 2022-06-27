dea58110365a547aa2665d9d5f97e8bcf9aa4f70
Muhammad Arsalan <arsalanmhussain32@gmail.com>
2022-06-27 23:16:38 +0500

    allow all origins

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ connectDB();
 app.use(credentials);
 
 // Cross Origin Resource Sharing
-app.use(cors(corsOptions));
+app.use(cors());
 
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
