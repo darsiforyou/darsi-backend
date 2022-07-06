a33327fe982ad49f0960e320674abf42150abafa
Hassan Mujtaba <hassanmujtaba.techtory@gmail.com>
2022-07-06 17:29:34 +0500

    fix some issue in auth api

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,49 +4,69 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const handleLogin = async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password)
-    return res.status(401).json({ error: "email and password are required." });
-  const foundUser = await User.findOne({ email: email });
-  if (!foundUser) return res.status(401).json({ error: "email is Invalid" }); //Unauthorized
-  // evaluate password
-  const pass = foundUser.password;
-  const match = await bcrypt.compare(password, pass);
-  if (match) {
-    const role = foundUser.role;
-    // create JWTs
-    const accessToken = jwt.sign(
-      {
-        UserInfo: {
-          id: foundUser.id,
-          email: foundUser.email,
-          role,
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(401).json({
+        message: "email and password are required.",
+        data: {},
+      });
+    const foundUser = await User.findOne({ email: email });
+    if (!foundUser)
+      return res.status(401).json({
+        message: "email is Invalid",
+        data: {},
+      });
+    // evaluate password
+    const pass = foundUser.password;
+    const match = await bcrypt.compare(password, pass);
+    if (match) {
+      const role = foundUser.role;
+      // create JWTs
+      const accessToken = jwt.sign(
+        {
+          UserInfo: {
+            id: foundUser.id,
+            email: foundUser.email,
+            role,
+          },
         },
-      },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "1d" }
-    );
-    const refreshToken = jwt.sign(
-      { email: foundUser.email },
-      process.env.REFRESH_TOKEN_SECRET,
-      { expiresIn: "1d" }
-    );
-    // Saving refreshToken with current user
-    await User.findById(foundUser._id).update({
-      refreshToken,
-    });
-    res.cookie("jwt", refreshToken, {
-      httpOnly: true,
-      sameSite: "None",
-      secure: true,
-      maxAge: 24 * 60 * 60 * 1000,
-    });
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+      const refreshToken = jwt.sign(
+        { email: foundUser.email },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: "1d" }
+      );
+      // Saving refreshToken with current user
+      await User.findById(foundUser._id).update({
+        refreshToken,
+      });
+      res.cookie("jwt", refreshToken, {
+        httpOnly: true,
+        sameSite: "None",
+        secure: true,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
 
-    const { password, refreshToken: rftoken, ...others } = foundUser._doc;
+      const { password, refreshToken: rftoken, ...others } = foundUser._doc;
 
-    res.json({ ...others, accessToken });
-  } else {
-    res.sendStatus(401);
+      res.status(200).json({
+        message: "Successfully loged in.",
+        data: { ...others, accessToken },
+      });
+    } else {
+      res.status(401).json({
+        message: "Password did not match",
+        data: {},
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+      data: {},
+    });
   }
 };
 
