cd93d6566beb3da61d73889ddd1bc0b5791f0926
Arsalan Hussain <arsalanmhussain32@gmail.com>
2023-01-14 16:04:56 +0500

    paypro api response

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const path = require('path');
+const path = require("path");
 
-router.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+router.get("^/$|/index(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
 });
 
+router.get("/paypro/uis", async (req, res) => {
+  try {
+    const { username, password, csvinvoiceids } = req.query;
+
+    console.log({ username, password, csvinvoiceids });
+    return { username, password, csvinvoiceids };
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      data: {},
+    });
+  }
+});
 module.exports = router;
