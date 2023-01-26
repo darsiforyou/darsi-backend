594e17a76d02e5f6cbf353e5dd4f751a4cf1eb60
Arsalan Hussain <arsalanmhussain32@gmail.com>
2023-01-26 23:13:10 +0500

    redirect to success page

--- src/routes/root.js
+++ src/routes/root.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const Order = require("../models/order");
 
 router.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
@@ -9,8 +10,13 @@ router.get("^/$|/index(.html)?", (req, res) => {
 router.get("/paypro/uis", async (req, res) => {
   try {
     const { username, password, csvinvoiceids } = req.query;
+    const order = Order.findByIdAndUpdate(csvinvoiceids, {
+      paymentStatus: true,
+    });
 
     console.log({ username, password, csvinvoiceids });
+
+    res.redirect("https://darsi.pk/success");
     return { username, password, csvinvoiceids };
   } catch (err) {
     res.status(500).json({
