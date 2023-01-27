b093cd0b0a4cfae02ebd60a8e9d397ea0dccbde4
Arsalan Hussain <arsalanmhussain32@gmail.com>
2023-01-27 09:15:04 +0500

    paypro redirect

--- src/routes/root.js
+++ src/routes/root.js
@@ -7,22 +7,22 @@ router.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
 });
 
-// router.get("/paypro/uis", async (req, res) => {
-//   try {
-//     const { username, password, csvinvoiceids } = req.query;
-//     const order = Order.findByIdAndUpdate(csvinvoiceids, {
-//       paymentStatus: true,
-//     });
+router.get("/paypro/uis", async (req, res) => {
+  try {
+    const { username, password, csvinvoiceids } = req.query;
+    const order = Order.findByIdAndUpdate(csvinvoiceids, {
+      paymentStatus: true,
+    });
 
-//     console.log({ username, password, csvinvoiceids });
+    console.log({ username, password, csvinvoiceids });
 
-//     res.redirect("https://darsi.pk/success");
-//     return { username, password, csvinvoiceids };
-//   } catch (err) {
-//     res.status(500).json({
-//       message: err.message,
-//       data: {},
-//     });
-//   }
-// });
+    res.redirect("https://darsi.pk/success");
+    return { username, password, csvinvoiceids };
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      data: {},
+    });
+  }
+});
 module.exports = router;
