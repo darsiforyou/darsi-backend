5bf4442f50fffc5c5980b797648b1846e1245eb1
Arsalan Hussain <arsalanmhussain32@gmail.com>
2023-02-02 14:07:31 +0500

    payment urls for refereal and product

--- src/routes/root.js
+++ src/routes/root.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const Order = require("../models/order");
+const User = require("../models/user");
 
 router.get("^/$|/index(.html)?", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
 });
 
-router.get("/paypro/uis", async (req, res) => {
+router.get("/paypro/product", async (req, res) => {
   try {
     const { username, password, csvinvoiceids } = req.query;
     console.log();
@@ -18,6 +19,26 @@ router.get("/paypro/uis", async (req, res) => {
     console.log({ username, password, csvinvoiceids });
 
     res.redirect("https://darsi.pk/success");
+    return { username, password, csvinvoiceids };
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+      data: {},
+    });
+  }
+});
+
+router.get("/paypro/referral", async (req, res) => {
+  try {
+    const { username, password, csvinvoiceids } = req.query;
+    console.log();
+    const user = User.findByIdAndUpdate(csvinvoiceids, {
+      referral_payment_status: true,
+    });
+
+    console.log({ username, password, csvinvoiceids });
+
+    res.redirect("https://dashboard.darsi.pk/login");
     return { username, password, csvinvoiceids };
   } catch (err) {
     res.status(500).json({
