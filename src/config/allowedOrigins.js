41a69d7bbd63d341a783da4eefdc07aa6774ccf6
fazzyA <faz.pak@gmail.com>
2023-09-08 23:45:09 +0500

    allowed origin

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,10 +1,10 @@
 const allowedOrigins = [
-  "https://www.yoursite.com",
   "http://127.0.0.1:5500",
   "http://localhost:3000",
   "http://localhost:3001",
   "https://darsi.pk",
   "https://dashboard.darsi.pk",
+  "https://darsi-admin.vercel.app",
   "*",
 ];
 
