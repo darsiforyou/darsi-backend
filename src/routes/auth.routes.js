131596fc374ab18f53091afc3fc730cc0b4e4bb4
darsiforyou <126070632+darsiforyou@users.noreply.github.com>
2025-12-24 20:53:26 +0500

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,13 +6,13 @@ const router = express.Router();
 
 // 🔹 GOOGLE LOGIN
 router.get(
-  "/google",
+  "api/auth/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
 );
-
+api/auth/google/callback
 // 🔹 CALLBACK
 router.get(
-  "/google/callback",
+  "api/auth/google/callback",
   passport.authenticate("google", { session: false }),
   (req, res) => {
     const user = req.user;
