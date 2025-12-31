cac40191f19a993cc8f51dcc2a0a60ca7fe977a3
darsiforyou <darsiforyou@gmail.com>
2025-12-31 16:17:56 +0500

    verfiy google token

--- src/middleware/verifyJWT.js
+++ src/middleware/verifyJWT.js
@@ -1,19 +1,19 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
 
-const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization || req.headers.Authorization;
-  if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403); //invalid token
-    req.userId = decoded.UserInfo.id;
-    req.user = decoded.UserInfo.email;
-    req.roles = decoded.UserInfo.role;
-    next();
-  });
-};
- module.exports = verifyJWT;
+// const verifyJWT = (req, res, next) => {
+//   const authHeader = req.headers.authorization || req.headers.Authorization;
+//   if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
+//   const token = authHeader.split(" ")[1];
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) return res.sendStatus(403); //invalid token
+//     req.userId = decoded.UserInfo.id;
+//     req.user = decoded.UserInfo.email;
+//     req.roles = decoded.UserInfo.role;
+//     next();
+//   });
+// };
+//  module.exports = verifyJWT;
 
 // const jwt = require("jsonwebtoken");
 
@@ -30,3 +30,33 @@ const verifyJWT = (req, res, next) => {
 // }
 
 // module.exports = verifyToken;
+
+
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization || req.headers.Authorization;
+  if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
+
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403); // invalid token
+
+    // Support both formats: { UserInfo: {...} } or flat { id, email, role }
+    if (decoded.UserInfo) {
+      req.userId = decoded.UserInfo.id;
+      req.user = decoded.UserInfo.email;
+      req.roles = decoded.UserInfo.role;
+    } else {
+      req.userId = decoded.id;
+      req.user = decoded.email;
+      req.roles = decoded.role;
+    }
+
+    next();
+  });
+};
+
+module.exports = verifyJWT;
